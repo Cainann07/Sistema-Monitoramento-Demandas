94a48e4539c877b5854e07136c23cf237d7840cc
Cainann07 <mateuscainan14@gmail.com>
2026-05-12 21:36:49 -0300

    Primeiro commit

---
+++ sistema-monitoramento-demandas/src/main/java/com/grupo2/sistemamonitoramento/SistemaMonitoramentoDemandasApplication.java
@@ -0,0 +1,13 @@
+package com.grupo2.sistemamonitoramento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMonitoramentoDemandasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMonitoramentoDemandasApplication.class, args);
+	}
+
+}
---
+++ sistema-monitoramento-demandas/src/test/java/com/grupo2/sistemamonitoramento/SistemaMonitoramentoDemandasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.sistemamonitoramento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMonitoramentoDemandasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8b859a230b2a4d75f52f05545d938dbdcb7e1264
Cainann07 <mateuscainan14@gmail.com>
2026-05-17 20:06:39 -0300

    Adiciona propriedades em memória para teste de contexto do Spring

--- back-end/src/test/java/com/grupo2/sistemamonitoramento/SistemaMonitoramentoDemandasApplicationTests.java
+++ back-end/src/test/java/com/grupo2/sistemamonitoramento/SistemaMonitoramentoDemandasApplicationTests.java
@@ -3,7 +3,14 @@ package com.grupo2.sistemamonitoramento;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// Forçando o teste de contexto a usar propriedades falsas em memória, evitando que ele procure o MySQL local
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password="
+})
+
 class SistemaMonitoramentoDemandasApplicationTests {
 
 	@Test
